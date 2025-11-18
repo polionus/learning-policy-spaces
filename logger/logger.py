a4b0b2b3318442e9837d7c88388bf623b2b8600b
Armin Ashrafi <ashrafi2@ualberta.ca>
2025-11-18 12:40:28 -0700

    feat: make logger easily importable

--- logger/logger.py
+++ logger/logger.py
@@ -2,26 +2,27 @@
 import sys
 from loguru import logger
 
-def setup_logger(log_file="run.log", level="INFO"):
-    logger.remove()
-    
-    # Console logger
-    logger.add(
-        sys.stderr,
-        level=level,
-        format="<green>{time:HH:mm:ss}</green> "
-               "| <level>{level}</level> "
-               "| <cyan>{file}</cyan>:<cyan>{line}</cyan> "
-               "| <level>{message}</level>",
-    )
-    
-    # File logger
-    logger.add(
-        log_file,
-        level="DEBUG",        # file gets everything
-        format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {file}:{line} | {message}",
-        rotation="10 MB",
-        enqueue=True,
-    )
-    
-    return logger
+# Remove any pre-existing handlers (important for notebooks / repeated imports)
+logger.remove()
+
+# Console logger
+logger.add(
+    sys.stderr,
+    level="INFO",
+    format="<green>{time:HH:mm:ss}</green> "
+           "| <level>{level}</level> "
+           "| <cyan>{file}</cyan>:<cyan>{line}</cyan> "
+           "| <level>{message}</level>",
+)
+
+# File logger
+logger.add(
+    "logger/run.log",
+    level="DEBUG",  # file gets everything
+    format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {file}:{line} | {message}",
+    rotation="10 MB",
+    enqueue=True,
+)
+
+# Export the configured logger
+__all__ = ["logger"]
