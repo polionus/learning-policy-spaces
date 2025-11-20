c02b18bb440b6a5ff82d77c0c38451afc666111d
Armin Ashrafi <ashrafi2@ualberta.ca>
2025-11-20 15:47:03 -0700

    chore: revert to older method

--- slurm.py
+++ slurm.py
@@ -3,71 +3,14 @@ import subprocess
 from time import sleep
 from termcolor import colored
 import hashlib
-import tyro
 
 
 
-ARCHIVE_NAME = "learning_policy_spaces_env.tar.gz"
-VENV_DIR = "cached_env"
+ARCHIVE_NAME = "$SLURM_TMPDIR/learning_policy_spaces_env.tar.gz"
+VENV_DIR = "$SLURM_TMPDIR/cached_env"
 
 def get_hash_name(cmd: str, length: int = 7):
     return f"exp_{hashlib.sha256(cmd.encode()).hexdigest()[:length]}"
-
-
-def generate_setup_script():
-    return f"""#!/bin/bash
-
-echo "Removing Old Archive ..."
-rm -rf {ARCHIVE_NAME}
-rm -rf {VENV_DIR}
-
-module load python/3.10
-
-# Create the env
-echo "Creating uv enviornment..."
-uv venv {VENV_DIR}
-
-
-#Activate the env
-echo "Activating the enviornment ..."
-source {VENV_DIR}/bin/activate
-
-
-
-#Install dependencies
-echo "Installing dependencies..."
-uv pip install -r requirements.txt
-
-# Deactivate
-echo "Deactivating the environment ..." 
-deactivate
-
-
-# Compress the enviornment
-echo "Compressing the environment ..."
-tar -cvzf {ARCHIVE_NAME} {VENV_DIR}
-
-# Clean up source directory
-echo "Cleaning up the source directory ..."
-rm -rf {VENV_DIR}
-
-echo "Setup Successful!"
-
-"""
-
-def setup_env(setup_script: str):
-
-    with tempfile.NamedTemporaryFile(delete=False, mode='w', suffix=".sh") as f:
-        f.write(setup_script)
-        setup_path = f.name
-
-    sleep(0.001)
-    activate_cmd = f"chmod +x {setup_path}".split()
-    run_command = [setup_path]
-
-    subprocess.run(activate_cmd, check = True)
-    subprocess.run(run_command, check = True)
-
 
 def generate_slurm_script(cmd, 
                           gpu_flag: bool, 
@@ -89,7 +32,8 @@ def generate_slurm_script(cmd,
 
 module load python/3.10
 echo "Copying cached environment '{ARCHIVE_NAME}' to fast storage ($SLURM_TMPDIR)..."
-cp -f $SLURM_SUBMIT_DIR/{ARCHIVE_NAME} $SLURM_TMPDIR/
+uv pip install -r requirements.txt
+
 
 #Important magic to make neural networks fast. Will not work with multi-threading
 export FLEXIBLAS=imkl
@@ -124,12 +68,7 @@ def submit(cmd,
     subprocess.run(['sbatch', slurm_path])
 
 
-def main(setup: bool = False):
-
-    if setup:
-        setup_script = generate_setup_script()
-        setup_env(setup_script)
-        return
+def main():
 
     cmd = input(colored("Please input your command: ", 'red'))
     gpu_flag = input(colored("GPU?: ", 'blue'))
@@ -148,4 +87,4 @@ def main(setup: bool = False):
             )
     
 if __name__ == "__main__":
-    tyro.cli(main)
+    main()
