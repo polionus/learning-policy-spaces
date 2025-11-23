dec2eaaba7ca9a37b993569638a16a9c233ec135
Armin Ashrafi <ashrafi2@ualberta.ca>
2025-11-22 18:12:47 -0700

    wip: develop cleaner methods for latent search

--- search/search_tools.py
+++ search/search_tools.py
@@ -2,25 +2,62 @@ import jax.numpy as jnp
 from logger.logger import logger
 from dsl import DSL
 from config import SearchConfig
+from dataclasses import dataclass
 from functools import partial
-from typing import List
+from typing import List, Callable
 from utils.program import execute_program 
 from multiprocessing import Pool
 import equinox as eqx
 import jax
 
 
-def get_neighbors(sigma: float, new_indices:jax.Array, elite_population: jax.Array, key: jax.Array):
-    mu = elite_population[new_indices]
+@dataclass
+class SearchState:
+
+    population: jax.Array
+    converged: bool
+    counter_for_restart: int
+    best_reward: float
+    best_program: str
+    prev_mean_elite_reward: float
+    sigma: float
+
+def get_search_method(method: str) -> Callable:
+    if method == "CEM":
+        if SearchConfig.cem_reduce_to_mean:
+            return CEM_MEAN
+        else: 
+            return CEM
+    elif method == "CEBS":
+        return CEBS
+    else: 
+        raise ValueError(f"Expected `CEM` or `CEBS` as search methods, but got {method}")
+
+def init_search_state() -> SearchState: 
+
+    search_state = SearchState(population = init_population(),
+                                converged = False,
+                                num_evaluations = 0,
+                                counter_for_restart = 0,
+                                best_reward = -float("inf"),
+                                best_program = None,
+                                prev_mean_elite_reward = -float("inf"))
+
+    return search_state 
+
+def get_neighbors(sigma: float, elite_choice_indices:jax.Array, elite_population: jax.Array, key: jax.Array):
+    mu = elite_population[elite_choice_indices]
     population = jax.random.normal(key, shape= mu.shape) + sigma
     return population
 
 def search_sigma_anneal(sigma: float, sigma_min: float, sigma_rate: float):
     sigma = max(sigma_min, sigma * sigma_rate)
+    return sigma
 
 def init_population(latent_size: int, population_size: int, key: jax.Array) -> jax.Array:
     return jax.random.normal(key, shape = (population_size, latent_size))
 
+### The logic is basically the same, in our version we are simply going to make sure 
 
 def execute_population(
         model: eqx.Module, population: jax.Array, dsl: DSL, task_envs: List
@@ -86,3 +123,41 @@ def process_population_results(results, dsl: DSL, best_reward: float, num_evalua
             break
 
     return jnp.array(rewards), best_reward, num_evaluations, best_program, converged
+
+
+def maybe_continue_population(mean_elite_reward: float, search_state: SearchState, elite_population: jax.Array, key: jax.Array):
+
+    #Update counter
+    search_state.counter_for_restart += mean_elite_reward == search_state.prev_mean_elite_reward
+    if search_state.counter_for_restart >= SearchConfig.restart_timeout and SearchConfig.restart_timeout > 0:
+                search_state.population = init_population()
+                search_state.counter_for_restart = 0
+                search_state.sigma = SearchConfig.initial_sigma
+                # StdoutLogger.log("Latent Search", "Restarted population.")
+    else:             
+        elite_choice_indices, elite_population = SearchConfig.search_method(elite_population, key)    
+        search_state.population = get_neighbors(elite_choice_indices, elite_population)
+
+        # Anneal Sigma only when actually continuing search?
+        # Should I not actually reset sigma?
+        # I should actually resent the sgima
+        search_state.sigma = search_sigma_anneal(SearchState.sigma, SearchConfig.sigma_min, SearchConfig.sigma_decay_rate)
+             
+    return search_state
+
+def CEM_MEAN(elite_population: jax.Array, key: jax.Array):
+
+    elite_choice_indices = jnp.zeros(SearchConfig.population_size, dtype = jnp.int16) #This just works!
+    mean_elite = jnp.mean(elite_population, axis=0)       
+    #Replace elite population with its mean
+    elite_population = elite_population = jnp.tile(mean_elite, (SearchConfig.n_elite, 1))
+    return elite_choice_indices, elite_population
+
+def CEM(elite_population: jax.Array, key: jax.Array):
+    ###choose n_elites randomly.
+    elite_choice_indices = jax.random.choice(key, SearchConfig.n_elite, replace=True)
+    return elite_choice_indices, elite_population
+
+def CEBS(elite_population: jax.Array, key: jax.Array):
+    elite_choice_indices = jnp.arange(SearchConfig.n_elite).repeat(int(SearchConfig.population_size, SearchConfig.n_elite))
+    return elite_choice_indices, elite_population
