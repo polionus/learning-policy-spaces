9484222f46d72bf900a238634ce6fee36bb65800
Armin Ashrafi <ashrafi2@ualberta.ca>
2025-11-23 17:35:01 -0700

    feat: add aim experiment tracker

--- main_latent_search.py
+++ main_latent_search.py
@@ -12,17 +12,5 @@ from utils.rng import make_key_gen
 
 
     task_cls = get_task_cls(Config.env_task)
-    params = torch.load(Config.model_params_path(), map_location=Runtime.device)
-    model.load_state_dict(params, strict=False)
-    searcher = get_latent_search()(model, task_cls, dsl)
-
-    StdoutLogger.log(
-        "Main",
-        f"Starting Latent Search with model {Config.model_name} for task {Config.env_task}",
-    )
-
-    best_program, converged, num_evaluations = searcher.search()
-
-    StdoutLogger.log("Main", f"Converged: {converged}")
-    StdoutLogger.log("Main", f"Final program: {best_program}")
-    StdoutLogger.log("Main", f"Number of evaluations: {num_evaluations}")
+    run = Run()
+    searcher = LatentSearch(model, task_cls, dsl, run)
