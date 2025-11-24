c72b92ed65fa96d24d1b66fef6f1db14e5d81987
Armin Ashrafi <ashrafi2@ualberta.ca>
2025-11-23 17:37:58 -0700

    wip: restructure search algorithm

--- search/latent_search.py
+++ search/latent_search.py
@@ -1,58 +1,19 @@
 from __future__ import annotations
-from typing import Callable
-import time
-import torch
 import jax
 import jax.numpy as jnp
-import search_tools as st
+from . import search_tools as st
 from utils.program import execute_program
 from dsl import DSL
-from multiprocessing import Pool
-from functools import partial
 from logger.logger import logger
 
 from vae.models.base_vae import BaseVAE
-from config import SearchConfig, TrainConfig, Config
+from config import SearchConfig, TrainConfig
 from tasks.task import Task
 from aim import Run
 
 
 #TODO: Change this to a eqx.Module for jaxification.
-def search(n_elite: int, number_iterations: int, search_method: Callable, key: jax.Array) -> tuple[str, bool, int]:
-        """Main search method. Searches for a program using the specified DSL that yields the
-        highest reward at the specified task.
 
-        Returns:
-            tuple[str, bool]: Best program in string format and a boolean value indicating
-            if the search has converged.
-        """
-
-        ### initialize search
-        search_state = st.init_search_state()
-       
-        for iteration in range(1, number_iterations + 1):
-            rewards = st.execute_population(search_state.population)
-
-            if search_state.converged:
-                break
-
-            _, topk_indices = jax.lax.top_k(rewards, n_elite)
-            elite_population = search_state.population[topk_indices]
-            mean_elite_reward = jnp.mean(rewards[topk_indices])
-
-            logger.info(
-                "Latent Search",
-                f"Iteration {iteration} mean elite reward: {mean_elite_reward}",
-            )
-            
-            search_state = st.maybe_continue_population(mean_elite_reward, search_state, elite_population, key)
-            search_state.prev_mean_elite_reward = mean_elite_reward
-            
-
-        if not search_state.converged:
-            ## TODO: Gotta track the best reward here.
-            pass
-        return search_state
 
 class LatentSearch:
     """Implements the CEM method from LEAPS paper."""
@@ -62,11 +23,55 @@ class LatentSearch:
         
         self.run = run
         self.dsl = dsl
-        self.search_method = st.get_search_method()   
+        self.search_method = st.get_search_method(SearchConfig.search_method_name)   
         self.model_hidden_size = TrainConfig.hidden_size
 
         ### Understand and correct this
-        self.task_envs = [task_cls(i) for i in range(SearchConfig.number_executions)]
+        self.task_envs = [task_cls(i) for i in range(SearchConfig.num_env_executions)]
+        # jax.debug.breakpoint()
+
+
+    def search(self,  key: jax.Array) -> tuple[str, bool, int]:
+        """Main search method. Searches for a program using the specified DSL that yields the
+        highest reward at the specified task.
+
+        Returns:
+            tuple[str, bool]: Best program in string format and a boolean value indicating
+            if the search has converged.
+        """
+
+        ### initialize search
+        search_state = st.init_search_state(key)
+        for iteration in range(1, SearchConfig.num_iterations + 1):
+            results = st.execute_population(self.model, search_state.population, self.dsl, self.task_envs)
+            raise NotImplementedError('The rest of this function still needs to be developed.')
+            rewards = [reward for _,_,reward in results]
+
+            if search_state.converged:
+                break
+            
+            _, topk_indices = jax.lax.top_k(rewards, SearchConfig.n_elite)
+            elite_population = search_state.population[topk_indices]
+            mean_elite_reward = jnp.mean(rewards[topk_indices])
+
+            logger.info(
+                "Latent Search",
+                f"Iteration {iteration} mean elite reward: {mean_elite_reward}",
+            )
+            
+            search_state = st.maybe_continue_population(mean_elite_reward, 
+                                                        search_state, 
+                                                        elite_population, 
+                                                        self.search_method,
+                                                        key)
+            search_state.prev_mean_elite_reward = mean_elite_reward
+            
+
+        if not search_state.converged:
+            ## TODO: Gotta track the best reward here.
+            pass
+        return search_state
+
        
 
 
