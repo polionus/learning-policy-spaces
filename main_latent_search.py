76d1fd93e857f1bcec5a727896d369c377c4c536
Armin Ashrafi <ashrafi2@ualberta.ca>
2025-11-23 17:35:59 -0700

    chore: clean up main_trainer.py file

--- main_latent_search.py
+++ main_latent_search.py
@@ -9,11 +9,22 @@ from tasks import get_task_cls
 from aim import Run
 import jax
 from utils.rng import make_key_gen
-
+if __name__ == '__main__':
+    
+    dsl = DSL.init_default_karel()
+    
+    
     keygen = make_key_gen(jax.random.key(SearchConfig.seed))
+
     model_template = load_model(TrainConfig.model_name, dsl) #This loads the PyTree
     model = load_model_from_template(ArtifactConfig.model_params_path, model_template)
 
     task_cls = get_task_cls(Config.env_task)
     run = Run()
     searcher = LatentSearch(model, task_cls, dsl, run)
+    
+    logger.info('Main', f'Starting Latent Search with model {TrainConfig.model_name} for task {Config.env_task}')
+    
+    search_state = searcher.search(key = next(keygen))
+    
+    logger.info("Search Done")
