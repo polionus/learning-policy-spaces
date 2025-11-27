a373b9cbd2205e2c6bf4ed1656abcd5a5a0a30b4
Armin Ashrafi <ashrafi2@ualberta.ca>
2025-11-27 12:34:07 -0700

    feat: make main_syntax_checker work with jax

--- main_syntax_checker.py
+++ main_syntax_checker.py
@@ -1,33 +1,19 @@
-import torch
+import jax.numpy as jnp
 from dsl import DSL
 from dsl.syntax_checker import PySyntaxChecker
 
+if __name__ == '__main__':
 
-
-
-
-
-def main():
     dsl = DSL.init_default_karel()
-    sample_program = [0, 1, 2, 49, 32, 41, 33, 47, 31, 13, 9, 8, 10, 4, 4, 48, 3]
-
-    syntax_checker = PySyntaxChecker(dsl.t2i, torch.device('cpu'))
-    initial_state = syntax_checker.get_initial_checker_state()
-    state, sequence_mask = syntax_checker.get_sequence_mask(
-        initial_state, sample_program
-    )
-   
-    raise Exception('You have not finished implementing this to jax.')
-    # for idx, token in enumerate(sample_program):
-    #     valid_tokens = torch.where(sequence_mask[idx] == 0)[0]
-    #     valid_tokens = [
-    #         dsl.i2t[tkn.detach().cpu().numpy().tolist()] for tkn in valid_tokens
-    #     ]
-    #     valid_tokens = " ".join(valid_tokens)
-    #     print("valid tokens for {}: {}".format(dsl.i2t[token], valid_tokens))
-
-
-if __name__ == "__main__":
-    main()
-
     
+    sample_program = jnp.array([0, 1, 2, 49, 32, 41, 33, 47, 31, 13, 9, 8, 10, 4, 4, 48, 3])
+
+    syntax_checker = PySyntaxChecker(dsl.t2i)
+    initial_state = syntax_checker.get_initial_checker_state()
+    grammar_state, sequence_mask = syntax_checker.get_sequence_mask(initial_state, sample_program)
+    
+    for idx, token in enumerate(sample_program):
+        valid_tokens = jnp.where(sequence_mask[idx] == 0)[0]
+        valid_tokens = [dsl.i2t[tkn.tolist()] for tkn in valid_tokens]
+        valid_tokens = " ".join(valid_tokens)
+        print("valid tokens for {}: {}".format(dsl.i2t[token], valid_tokens))
