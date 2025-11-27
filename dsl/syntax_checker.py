fix: removed the problem with incorrect shapes making get_sequence mask not work. 0D and nD arrays will now work with the same logic.

dsl/syntax_checker.py
@@ -661,12 +661,15 @@ class PySyntaxChecker:
     ### Update: the input sequence has been written in such a way that the sequence mask is only passed once.
     def get_sequence_mask(self, state: CheckerState, inp_sequence: jax.Array) -> Tuple[CheckerState, jax.Array]:
 
-        def get_token_mask(checker_state: CheckerState, inp_token: int) -> Tuple[CheckerState, jax.Array]:
+        '''Unified function that gets input sequences and outputs the sequence mask and the updates grammar state. 0D inputs require jnp.expand_dims to work correctly  '''
 
+        def get_token_mask(checker_state: CheckerState, inp_token: int) -> Tuple[CheckerState, jax.Array]:
+        
             checker_state = self.forward(checker_state, inp_token)
             return checker_state, jnp.squeeze(self.allowed_tokens(checker_state))
-        
-        final_state, mask =  jax.lax.scan(get_token_mask, state, inp_sequence)
+
+        # # NOTE: Should unroll be true or false?
+        final_state, mask =  jax.lax.scan(get_token_mask, state, inp_sequence, unroll=False)
         return final_state, mask
 
     def get_initial_checker_state(self):

vae/models/base_vae.py
@@ -175,8 +175,8 @@ class BaseVAE(eqx.Module):
     def get_syntax_mask(
         self, current_token: jax.Array, grammar_state: CheckerState
     ):
-        
-        grammar_state, sequence_mask = self.syntax_checker.get_sequence_mask(grammar_state, [current_token])
+        current_token = jnp.expand_dims(current_token, axis = 0)
+        grammar_state, sequence_mask = self.syntax_checker.get_sequence_mask(grammar_state, current_token)
         syntax_mask = jnp.where(
                 sequence_mask,                                # boolean mask
                 -jnp.finfo(jnp.float32).max * jnp.ones_like(sequence_mask, dtype=jnp.float32),