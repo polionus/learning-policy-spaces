7dc996b485f71f27cb4e2a99ae4db9b8a2cf4870
Armin Ashrafi <ashrafi2@ualberta.ca>
2025-11-30 13:06:01 -0700

    chore: clean up the loss function

--- utils/losses.py
+++ utils/losses.py
@@ -16,7 +16,6 @@ class Batch(eqx.Module):
     progs_masks: jax.Array
 
 class Aux(NamedTuple):
-    output: jax.Array
     latent_loss: jax.Array
     progs_loss: jax.Array
     a_h_loss: jax.Array
@@ -61,8 +60,7 @@ def leaps_loss_fn(params, batch: Batch, static) -> Tuple[jax.Array, NamedTuple]:
     # The model takes in these parameters, and outputs: Teacher enforcing is enabled by default, and so was also enabled for us.
     model = eqx.combine(params, static)
     # jax.debug.breakpoint()
-    vmapped_model = eqx.filter_vmap(eqx.filter_vmap(model, in_axes=(inner_axes,)), in_axes=(outer_axes,))
-    output = vmapped_model(batch)
+    output = model(batch)
     # These.
 
     (   mu,
@@ -75,34 +73,35 @@ def leaps_loss_fn(params, batch: Batch, static) -> Tuple[jax.Array, NamedTuple]:
         pred_a_h_masks,
     ) = output
 
-    
-    # Combine first 2 dimensions of a_h (batch_size and demos_per_program) [NOT NEEDED ANYMORE WITH VMAP]
+    # Combine first 2 dimensions of a_h (batch_size and demos_per_program)
+    a_h = jnp.reshape(batch.a_h, (-1, batch.a_h.shape[-1]))#.view(-1, a_h.shape[-1])
+    a_h_masks = jnp.reshape(batch.a_h_masks, (-1, batch.a_h_masks.shape[-1]))#.view(-1, a_h_masks.shape[-1])
 
     # Skip first token in ground truth sequences
-    a_h = batch.a_h[1:]
-    a_h_masks = batch.a_h_masks[1:]
+    a_h = a_h[:, 1:]
+    a_h_masks = a_h_masks[:, 1:]
 
     # Flatten everything for loss calculation
     a_h_flat = jnp.reshape(a_h, (-1, 1))
     a_h_masks_flat = jnp.reshape(a_h_masks, (-1, 1))
 
     # Skip first token in ground truth sequences
-    progs = batch.progs[1:]
-    progs_masks = batch.progs_masks[1:]
+    progs = batch.progs[:, 1:]
+    progs_masks = batch.progs_masks[:, 1:]
 
     # Flatten everything for loss calculation
     progs_flat = jnp.reshape(progs, (-1, 1))
     progs_masks_flat = jnp.reshape(progs_masks, (-1, 1))
 
     
-    # pred_progs_logits = jnp.reshape(pred_progs_logits, (-1, pred_progs_logits.shape[-1])) #.view(-1, pred_progs_logits.shape[-1])
+    pred_progs_logits = jnp.reshape(pred_progs_logits, (-1, pred_progs_logits.shape[-1])) #.view(-1, pred_progs_logits.shape[-1])
     pred_progs_masks_flat = jnp.reshape(pred_progs_masks, (-1, 1))#.view(-1, 1)
+    # We combine masks here to penalize predictions that are larger than ground truth
     progs_masks_flat_combined = jnp.squeeze(jnp.maximum(
         progs_masks_flat, pred_progs_masks_flat
     ))
 
-    
-
+    pred_a_h_logits = jnp.reshape(pred_a_h_logits, (-1, pred_a_h_logits.shape[-1]))#.view(-1, pred_a_h_logits.shape[-1])
     pred_a_h_masks_flat = jnp.reshape(pred_a_h_masks, (-1 , 1))#.view(-1, 1)
     # We combine masks here to penalize predictions that are larger than ground truth
     a_h_masks_flat_combined = jnp.squeeze(jnp.maximum(
@@ -110,23 +109,16 @@ def leaps_loss_fn(params, batch: Batch, static) -> Tuple[jax.Array, NamedTuple]:
     ))
 
     # Calculate classification loss only on tokens in mask
-    zero_array = jnp.array([0.0])
-    progs_loss, a_h_loss = zero_array, zero_array
+    progs_loss, a_h_loss = 0, 0
 
-    ### In order for jit to work, I need to change these masks in a way that does not use advances indexing, but instead uses the masking as a computation that does not change the shape of the 
-    ### input dynamically.
     mask = progs_masks_flat_combined.astype(jnp.float32)
-
-    ### So we want the mask to make the logits of the things that are masked ineffective. 
-    ### Where does the mask come from?
-
     progs_loss = cross_entropy_loss(
         pred_progs_logits,
         jnp.reshape((progs_flat), -1),
         mask
     )
 
-    a_h_masks_flat_combined = a_h_masks_flat_combined.astype(jnp.float32)
+    # a_h_masks_flat_combined = a_h_masks_flat_combined.astype(jnp.float32)
     a_h_loss = cross_entropy_loss(
         pred_a_h_logits,
         jnp.reshape(a_h_flat, -1),
@@ -135,13 +127,13 @@ def leaps_loss_fn(params, batch: Batch, static) -> Tuple[jax.Array, NamedTuple]:
 
     latent_loss = model.get_latent_loss(mu, sigma)
 
-    ###TODO: we just remove the programmatic loss
     total_loss = (
         TrainConfig.prog_loss_coeff * progs_loss
         + TrainConfig.a_h_loss_coeff * a_h_loss
         + TrainConfig.latent_loss_coeff * latent_loss
     )
-    return total_loss, Aux(output=output, latent_loss=latent_loss, progs_loss=progs_loss, a_h_loss=a_h_loss)
+
+    return total_loss, Aux(latent_loss=latent_loss, progs_loss=progs_loss, a_h_loss=a_h_loss)
 
 
 
@@ -168,6 +160,7 @@ def strimmed_loss_fn(params, batch, static) -> Tuple[jax.Array, NamedTuple]:
         pred_a_h_masks,
     ) = output
 
+    
     
     # Combine first 2 dimensions of a_h (batch_size and demos_per_program)
     a_h = jnp.reshape(a_h, (-1, a_h.shape[-1]))#.view(-1, a_h.shape[-1])
